57cc795a35a9506921b76de86ac442757cd27956
ppvn <pop0vn3322@gmail.com>
2024-03-10 00:28:02 +0700

    доработан

--- 1.py
+++
@@ -1,8 +0,0 @@
-import local as l
-
-print(f'{l.text_tax_income}')
-amount = 0
-for i in range(0, 12):
-    value = float(input(f'{l.data_tax_income} {l.month[i]} {l.currency}'))
-    amount += value
-print(amount)
