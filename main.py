648722ed794a0a6715e6caaa31bf96052b7f7159
egor <egorstasyk@gmail.com>
2024-03-13 06:48:41 +0700

    main function fixed

--- main.py
+++ main.py
@@ -15,19 +15,19 @@ def single(x):
         sum_tax = 9075 * 0.1 + 0.15 * (x - 9075 - (x - 36900)) + 0.25 * (x - 36900)
     return sum_tax
 
-
-# print(l.tax_cat)
-# tax_var = int(input())
-# sum = income()
-# print(l.sum_income, sum)
-# sum -= TF_income()
-# print(l.sum_TF_income, sum)
-# match tax_var:
-#     case 1:
-#         single()
-#     case 2:
-#         duo()
-#     case 3:
-#         one_parent()
-#     case _:
-#         print(l.choice_error)
+def main():
+    print(l.tax_cat)
+    tax_var = int(input())
+    sum = income()
+    print(l.sum_income, sum)
+    sum -= TF_income()
+    print(l.sum_TF_income, sum)
+    match tax_var:
+        case 1:
+            single()
+        # case 2:
+        #     duo()
+        # case 3:
+        #     one_parent()
+        # case _:
+        #     print(l.choice_error)
