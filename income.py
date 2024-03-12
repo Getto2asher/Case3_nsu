678c940134bb8b24834d63091db83e92f4c80734
egor <egorstasyk@gmail.com>
2024-03-12 23:12:09 +0700

    income def

--- income.py
+++ income.py
@@ -1,8 +1,8 @@
 import local as l
-
-print(f'{l.text_income}')
-amount = 0
-for i in range(0, 12):
-    value = float(input(f'{l.data_income} {l.month[i]} {l.currency}'))
-    amount += value
-print(amount)
+def income():
+    print(f'{l.text_income}')
+    amount = 0
+    for i in range(0, 12):
+        value = float(input(f'{l.data_income} {l.month[i]} {l.currency}'))
+        amount += value
+    return amount
