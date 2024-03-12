a75b1440fec2c401ed880bdb9dded3ed14c1da43
egor <egorstasyk@gmail.com>
2024-03-12 23:00:49 +0700

    base

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 # Case-study #3
-# Developers: General Popov, Mr Stasuk.
+# Developers: General Popov, Egor A.
+
+import local
+
+
+print(local.tax_cat)
+tax_var = int(input())
+match tax_var:
+    case 1:
+        single()
+    case 2:
+        duo()
+    case 3:
+        one_parent()
+    case _:
+        print(local.choice_error)
+
+
