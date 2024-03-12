af483f34646c4b95565cf13ec9b149e55b3933d6
egor <egorstasyk@gmail.com>
2024-03-13 06:39:16 +0700

    single function added

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 # Case-study #3
 # Developers: General Popov, Egor A.
 
-import local
+import local as l
+from income import income
+from TF_income import TF_income
 
 
-print(local.tax_cat)
-tax_var = int(input())
-match tax_var:
-    case 1:
-        single()
-    case 2:
-        duo()
-    case 3:
-        one_parent()
-    case _:
-        print(local.choice_error)
+def single(x):
+    sum = income()
+    print(l.sum_income)
+    sum -= TF_income()
+    print(l.sum_TF_income)
+    x = int(x)
+    if x < 9076:
+        sum_tax = x * 0.1
+    if x >= 9076 and x <= 36900:
+        sum_tax = 9075 * 0.1 + 0.15 * (x - 9075)
+    else:
+        sum_tax = 9075 * 0.1 + 0.15 * (x - 9075 - (x - 36900)) + 0.25 * (x - 36900)
+    return sum_tax
 
 
+# print(l.tax_cat)
+# tax_var = int(input())
+# match tax_var:
+#     case 1:
+#         single()
+#     case 2:
+#         duo()
+#     case 3:
+#         one_parent()
+#     case _:
+#         print(l.choice_error)
